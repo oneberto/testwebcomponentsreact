50710d5fdddf44efdd8820cd182f598d74e3331f
Umberto Pontes <umberto.pontes@proenem.com.br>
2021-04-14 10:03:14 -0300

    add lib

---
+++ src/components.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'prodigio-components';
+
+
+
+export const ProButton = /*@__PURE__*/createReactComponent<JSX.ProButton, HTMLProButtonElement>('pro-button');
