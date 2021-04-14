9b85ff3cf2ee991e9609a4f210db43748e0904d0
Umberto Pontes <umberto.pontes@proenem.com.br>
2021-04-14 10:17:45 -0300

    test 3

---
+++ dist/components.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+export const ProButton = /*@__PURE__*/ createReactComponent('pro-button');
+//# sourceMappingURL=components.js.map
